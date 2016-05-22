01d743d2eab842cee13e173adc2697d9cb0f69f2
Antti Tönkyrä <daedalus@pingtimeout.net>
2016-05-23 02:28:47 +0300

    sometimes we want to read the file from the beginning :)

--- util/filemonitor.py
+++ util/filemonitor.py
@@ -13,13 +13,15 @@ class FileMonitor(object):
         self._size = None
         self._inode = None
         self._fd = None
+        self._read_from_beginning = False
 
     def open(self):
         self._fd = open(self._target, 'r')
         st_results = os.stat(self._target)
         self._size = st_results[6]
         self._inode = st_results.st_ino
-        self._fd.seek(self._size)
+        if not self._read_from_beginning:
+            self._fd.seek(self._size)
         self._position = self._fd.tell()
 
     def reopen(self):
@@ -34,15 +36,18 @@ class FileMonitor(object):
         st_results = os.stat(self._target)
         if st_results.st_ino != self._inode:
             logger.info('file rotated, reopening...')
+            self._read_from_beginning = True
             self.reopen()
         elif st_results[6] < self._position:
             logger.info('file truncated, reopening...')
+            self._read_from_beginning = True
             self.reopen()
 
     def read(self):
         try:
             self.check_file_sanity()
         except BaseException as be:
+            self._read_from_beginning = True
             logger.error(be)
             return
         line = self._fd.readline()
